04413379b0ff1c71fcdc4da0eb3f24bd066aac3e
Djordje Ristic <rist.djordje@gmail.com>
2023-11-30 23:27:56 -0300

    Adding Vercel Analytics to the project

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Layout, Hero, About, Jobs, Featured, Projects, Contact } from '@components';
 import { Education } from '../components';
+import { Analytics } from '@vercel/analytics/react';
 
 const StyledMainContainer = styled.main`
   counter-reset: section;
@@ -10,15 +11,17 @@ const StyledMainContainer = styled.main`
 
 const IndexPage = ({ location }) => (
   <Layout location={location}>
-    <StyledMainContainer className="fillHeight">
-      <Hero />
-      <About />
-      <Jobs />
-      <Education />
-      <Featured />
-      <Projects />
-      <Contact />
-    </StyledMainContainer>
+    <Analytics>
+      <StyledMainContainer className="fillHeight">
+        <Hero />
+        <About />
+        <Jobs />
+        <Education />
+        <Featured />
+        <Projects />
+        <Contact />
+      </StyledMainContainer>
+    </Analytics>
   </Layout>
 );
 
